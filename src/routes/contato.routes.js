ad7ed8145370a037e7e44f42b695432f0b411a4d
camisbersann <camila.bersan@aluno.senai.br>
2024-06-07 22:17:53 -0300

    feat: add coments in contato.routes.js

--- src/routes/contato.routes.js
+++ src/routes/contato.routes.js
@@ -1,8 +1,12 @@
+//Importa o módulo 'express' para criar rotas;
 const express = require('express');
+//Cria um objeto de rota do express;
 const router = express.Router();
 
+//Importa as funcões do controllers;
 const contatoController = require('../controllers/contatoController');
 
+//Define as rotas;
 router.post('/contatos', contatoController.addContato);
 router.put('/contatos', contatoController.updateContato);
 router.delete('/contatos', contatoController.deleteContato);
@@ -10,6 +14,5 @@ router.get('/contatos/:id', contatoController.getContatoById);
 router.get('/contatos/:email', contatoController.getContatoByEmail);
 router.get('/contatos', contatoController.getContato);
 
-
-
+//Exporta o router para ser utilizado em outras partes da aplicação;
 module.exports = router;
