771fdf48f13cd8a70916fd0c837370602e9b427f
camisbersann <camila.bersan@aluno.senai.br>
2024-06-07 22:21:57 -0300

    feat: add coments in login.routes.js

--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -1,8 +1,13 @@
+//Importa o módulo 'express' para criar rotas;
 const express = require('express');
+//Cria um objeto de rota do express;
 const router = express.Router();
+
+//Importa a função do controllers;
 const { loginUser } = require('../controllers/loginController');
 
 // Rota para login
 router.post('/login', loginUser);
 
+//Exporta o router para ser utilizado em outras partes da aplicação;
 module.exports = router;
