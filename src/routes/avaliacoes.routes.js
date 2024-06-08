25a543ce4e8c13d5499a036556797ff5ae048d61
camisbersann <camila.bersan@aluno.senai.br>
2024-06-07 21:40:15 -0300

    feat: add coments in avaliacoes.routes.js

--- src/routes/avaliacoes.routes.js
+++ src/routes/avaliacoes.routes.js
@@ -1,6 +1,13 @@
+//Importa o módulo 'express' para criar rotas;
 const express = require('express');
+
+//Cria um objeto de rota do express;
 const router = express.Router();
+
+//Importa o middleware de autenticação de token;
 const authenticateToken = require('../middleware/authMiddleware');
+
+//Importa o controllers;
 const { addReview, getReviewsByRestaurant } = require('../controllers/avaliacoesController');
 
 // Rota para adicionar uma nova avaliação
@@ -9,4 +16,5 @@ router.post('/review', authenticateToken, addReview);
 // Rota para obter todas as avaliações de um restaurante específico
 router.get('/reviews/:restaurantId', getReviewsByRestaurant);
 
+//Exporta o router para ser utilizado em outras partes da aplicação;
 module.exports = router;
