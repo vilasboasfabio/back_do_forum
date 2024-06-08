864a052a2a7c148e8e091dfcf25843c3e2f5cce7
camisbersann <camila.bersan@aluno.senai.br>
2024-06-07 21:09:37 -0300

    feat: add coments in loginController.js

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,12 +1,17 @@
+//Importa o módulo de configuração do banco de dados;
 const pool = require('../config/dbConfig');
+//Importa o módulo bcrypt para lidar com hash de senha;
 const bcrypt = require('bcrypt');
+//Importa o módulo jsonwebtoken para lidar com tokens JWT;
 const jwt = require('jsonwebtoken');
 
+//Função assíncrona para autenticar um usuário;
 const loginUser = async (req, res) => {
+  //Extrai os dados do corpo da requisição;
   const { email, password } = req.body;
 
   try {
-    // Check if user exists
+    //Verifica se o usuário existe no banco de dados;
     const userResult = await pool.query('SELECT * FROM Users WHERE Email = $1', [email]);
     if (userResult.rows.length === 0) {
       return res.status(401).json({ message: 'Invalid email or password' });
@@ -14,24 +19,26 @@ const loginUser = async (req, res) => {
 
     const user = userResult.rows[0];
 
-    // Verify password
+    //Verifica se a senha fornecida corresponde à senha armazenada no banco de dados;
     const validPassword = await bcrypt.compare(password, user.pass);
     if (!validPassword) {
       return res.status(401).json({ message: 'Invalid email or password' });
     }
 
-    // Create JWT token
+    //Cria um token JWT para o usuário autenticado;
     const token = jwt.sign({ userId: user.userid, email: user.email }, process.env.JWT_SECRET, {
-      expiresIn: '24h'
+      expiresIn: '24h'// Define a expiração do token para 24 horas;
     });
 
     res.json({ token });
   } catch (error) {
+    //Loga um erro em caso de falha ao autenticar o usuário e retorna uma resposta de erro;
     console.error('Error logging in:', error);
     res.status(500).send('Error logging in');
   }
 };
 
+//Exporta a função para ser utilizada em outras partes da aplicação;
 module.exports = {
   loginUser,
 };
