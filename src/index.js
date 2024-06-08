976ddd8068d5a59feb83f2da44a485444a48794f
camisbersann <camila.bersan@aluno.senai.br>
2024-06-07 22:31:36 -0300

    feat: add coments in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+//Importa e configura o dotenv para carregar variáveis de ambiente do arquivo .env;
 require('dotenv').config();
+//Importa o módulo 'express';
 const express = require('express');
+//Importa o módulo 'cors';
 const cors = require('cors');
+//Importa o módulo 'body-parser';
 const bodyParser = require('body-parser');
+
+//Importa as rotas;
 const usuariosRoutes = require('./routes/usuarios.routes.js');
 const restaurantesRoutes = require('./routes/restaurantes.routes.js');
 const avaliacoesRoutes = require('./routes/avaliacoes.routes.js');
@@ -10,17 +16,19 @@ const favoritosRoutes = require('./routes/favoritos.routes.js');
 const avaliacoesController = require('./routes/avaliacoes.routes.js');
 const contatosController =  require('./routes/contato.routes.js')
 
-
-
+//Cria uma instância do aplicativo Express;
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+
+//Define a porta do servidor;
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+//Define rotas para os diversos recursos da aplicação;
 app.use('/', favoritosRoutes);
 app.use('/', loginRoutes);
 app.use('/', usuariosRoutes);
@@ -29,7 +37,7 @@ app.use('/', avaliacoesRoutes);
 app.use('/', avaliacoesController);
 app.use('/', contatosController);
 
-
+//Inicia o servidor e "ouve" na porta especificada;
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port} 🥂`);
 });
