bba16a1778a80e47b1a3e37b788d94b5c3403899
camisbersann <camila.bersan@aluno.senai.br>
2024-06-07 22:35:53 -0300

    feat: add coments in dbConfig.js

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,11 +1,14 @@
+//Importa o módulo Pool para gerenciar conexões com o banco de dados;
 const { Pool } = require('pg');
 
+//Conjunto de conexões com o banco de dados;
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
+  user: process.env.DB_USER, 
+  host: process.env.DB_HOST, 
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
 
+//Exporta a instância do Pool para que ela possa ser utilizadas em outros módulos;
 module.exports = pool;
